b42ca1b81d44415d619d563e162314d4c1f943f9
Mattyzz <mdc0195@gmail.com>
2022-02-15 18:29:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <h1>Web Application for CSE4500</h1>
 <!-- Name --!>
 <h1>
-    <?php echo $json_data['first-name']; ?>
-    <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
+    <?php echo $my_data['first-name']; ?>
+    <span class="text-primary"><?php echo $my_data['last-name']; ?></span>
 </h1>
 <h2>Skills</h2>
 <h2>Interest</h2>
