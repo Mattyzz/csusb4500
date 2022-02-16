0a00978055889bcc33c118af555b57f95f4e125c
Mattyzz <mdc0195@gmail.com>
2022-02-15 18:32:49 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,21 @@ $json_data = json_decode($json_file,true);
 
 ?>
 <!-- Name --!>
-<h1>
+<h2>
     <?php echo $json_data['first-name']; ?>
     <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
-</h1>
+</h2>
 <h2>Skills</h2>
+<?php foreach($json_data['experience'] AS $experience) { ?>
+  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="flex-grow-1">
+      <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+      <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+      <p><?php echo $experience['description']; ?></p>
+    </div>
+    <div class="flex-shrink-0">
+      <span class="text-primary"><?php echo $experience['period']; ?></span>
+    </div>
+  </div>
+<?php  } ?>
 <h2>Interest</h2>
