08ee4761c5d8482d22af9fa914695e0310448b98
Mattyzz <mdc0195@gmail.com>
2022-02-15 18:31:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <h1>Web Application for CSE4500</h1>
+<?php
+
+// Read the JSON file
+$json_file = file_get_contents('my_data.json');
+
+// Decode the JSON file
+$json_data = json_decode($json_file,true);
+
+?>
 <!-- Name --!>
 <h1>
-    <?php echo $my_data['first-name']; ?>
-    <span class="text-primary"><?php echo $my_data['last-name']; ?></span>
+    <?php echo $json_data['first-name']; ?>
+    <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
 </h1>
 <h2>Skills</h2>
 <h2>Interest</h2>
